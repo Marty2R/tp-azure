0c0e7a31cdc6d7e0e5aad3c79468bda982d320be
Marty Rabatel <marty.rabatel2@gmail.com>
2025-05-27 11:19:29 +0200

    ajout page html

--- componants/Img.jsx
+++
@@ -1,8 +0,0 @@
-import my_image from "@/public/img/image.png";
-import Image from "next/image";
-
-function Img() {
-  return <Image src={my_image} width={500} height={300} alt="image" />;
-}
-
-export default Img;
